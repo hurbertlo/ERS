f659724270f59bad137b6843368a423bb85dcdf4
Kay Wong <enjoykayday@gmail.com>
2023-01-19 11:24:31 +0800

    add landing page

--- database/init_data.sql
+++ database/init_data.sql
@@ -9,7 +9,6 @@ INSERT INTO orders (ordered_by,address,total_price,status,created_at,updated_at)
 	(3,'Mong Kok',618.0,'Order Received','2023-01-02 00:00:00+08','2023-01-02 00:00:00+08');
 
 
-<<<<<<< HEAD
 INSERT INTO order_details (order_id,product_id,amount,price_per_item,discount_amount,price,created_at,updated_at) VALUES
 	 (1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
 	 (1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
@@ -17,21 +16,20 @@ INSERT INTO order_details (order_id,product_id,amount,price_per_item,discount_am
 	 (1,8,1,56.0,0.0,56.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
 	 (1,10,6,69.0,0.0,414.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
 	 (2,3,7,68.0,0.0,476.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	 (3,9,6,60.0,0.0,540.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08');
+		 (3,9,6,60.0,0.0,540.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08');
 
 -- KAY
 insert into chats (id, sender, receiver, content, content_type, created_at, updated_at) VALUES (1,1,2,'Why my product did not delivery on time?','text','2023-01-24','2023-01-31');
 insert into chats (id, sender, receiver, content, content_type, created_at, updated_at) VALUES (2,2,3,'How many stock of bell_bone do you have? ','text','2023-01-12','2023-01-19');
 insert into chats (id, sender, receiver, content, content_type, created_at, updated_at) VALUES (3,3,1,'Can you deliver the order by today?','text','2023-01-01','2023-01-08');
-=======
-	T INTO order_details (order_id,product_id,amount,price_per_item,discount_amount,price,created_at,updated_at) VALUES
-	(1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(1,5,2,258.0,0.0,516.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(1,8,1,56.0,0.0,56.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(1,10,6,69.0,0.0,414.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(2,3,7,68.0,0.0,476.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
-	(3,9,6,60.0,0.0,540.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08');
+	-- T INTO order_details (order_id,product_id,amount,price_per_item,discount_amount,price,created_at,updated_at) VALUES
+	-- (1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (1,2,4,60.0,0.0,240.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (1,5,2,258.0,0.0,516.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (1,8,1,56.0,0.0,56.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (1,10,6,69.0,0.0,414.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (2,3,7,68.0,0.0,476.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08'),
+	-- (3,9,6,60.0,0.0,540.0,'2023-01-02 00:00:00+08','2023-01-02 00:00:00+08');
 
 
 
@@ -66,4 +64,3 @@ INSERT INTO users (user_type_id, address, mobile, email, password, created_at, u
 INSERT INTO users (user_type_id, address, mobile, email, password, created_at, updated_at) VALUES ('sam', 'Kowloon Tong', '98628763', 'sam@hotmail.com', 'p@ss123', '2023-01-16', '2023-01-18');
 INSERT INTO users (user_type_id, address, mobile, email, password, created_at, updated_at) VALUES ('ken', 'Mong Kok', '58362837', 'ken@hotmail.com', 'pass456', '2023-01-16', '2023-01-18');
 
->>>>>>> 98281f979084a0a71ef2d4725f1467d48d8e4430
---
+++ public/header.js
@@ -0,0 +1,95 @@
+document.querySelector('#header').innerHTML = /*html*/`
+<nav class="navbar bg-light">
+<div class="container-fluid">
+   <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  <a class="navbar-brand" href="#">Pet World</a>
+  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Pet World</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+       
+        <div class="menu-container">
+            <div class="menu-item">首頁1</div>
+            <div class="menu-item">食品</div>
+            <div class="menu-item">衣服</div>
+            <div class="menu-item">玩具</div>
+            <div class="menu-item">工具</div>
+        </div>
+       
+    
+      <!-- <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul> -->
+
+      <!-- search bar
+    <form class="d-flex mt-3" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </div>
+    </div>
+    </div>
+</nav> 
+
+<div class="main-container">
+        <div class="head-container">
+        <img class="logo" src="https://i.pinimg.com/564x/d1/c7/d5/d1c7d52effae25506859147b6039b7fe.jpg"/>
+        <div class="icon-group">
+        <div class="icon-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+            </svg></i></div>
+        <div class="icon-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg></div>
+        <div class="icon-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+            </svg></div>
+        </div>
+        </div>
+
+
+
+
+        <div class="menu-container-2">
+        <div class="menu-item">首頁</div>
+        <div class="menu-item">食品</div>
+        <div class="menu-item">衣服</div>
+        <div class="menu-item">玩具</div>
+        <div class="menu-item">工具</div>
+        </div>
+</div>
+
+`
+
+
+window.addEventListener('scroll',()=>{
+    console.log(window.scrollY)
+    if (window.scrollY > 10){
+        document.querySelector('#header').classList.add('header-shadow')
+    }else{
+        
+        document.querySelector('#header').classList.remove('header-shadow')
+    }
+})
