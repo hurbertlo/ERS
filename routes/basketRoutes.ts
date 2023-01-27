67363314dd20766af0675de07c04e6e79d47ed49
Hurbert <hurbert.phlo@gmail.com>
2023-01-27 16:30:27 +0800

    rou

--- routes/basketRoutes.ts
+++ routes/basketRoutes.ts
@@ -20,27 +20,27 @@ export async function addToBasket(req: express.Request, res: express.Response) {
         let productId = Number(req.params.productId)
         let userId = req.session['userId']
         let selectedProduct = (await client.query(`select * from products where id = $1`, [Number(productId)])).rows[0]
-        let basketItem: any = (await client.query(`select * from baskets where order_by = $1 and  product_id = $2`, [userId, productId])).rows[0]
+        let basketItem: any = (await client.query(`select * from baskets where ordered_by = $1 and  product_id = $2`, [userId, productId])).rows[0]
 
         // update current record's quantity in basket
         if (basketItem) {
             console.log(Number(quantity));
             console.log(basketItem.quantity);
-            await client.query(`update baskets set quantity = $1 where order_by = $2`, [Number(quantity) + basketItem.quantity, userId])
+            await client.query(`update baskets set quantity = $1 where ordered_by = $2`, [Number(quantity) + basketItem.quantity, userId])
         } else {
             // add new record in basket
 
             await client.query(`
                 INSERT INTO baskets
-                (order_by, product_id, quantity )
+                (ordered_by, product_id, quantity )
                 VALUES($1, $2, $3);
             `, [userId, selectedProduct.id, Number(quantity)])
 
         }
 
-
-
-        res.end('ok')
+        res.json({
+            message: "Added successfully"
+        })
 
     } catch (error: any) {
         res.status(500).end(error.message)
@@ -49,14 +49,51 @@ export async function addToBasket(req: express.Request, res: express.Response) {
 }
 
 
-async function updateBasketItem(req: express.Request, res: express.Response) {
+export async function updateBasketItem(req: express.Request, res: express.Response) {
+    try {
+        let { productId, quantity } = req.body
+        let userId = req.session['userId']
+        let basketItem: any = (await client.query(`select * from baskets where ordered_by = $1 and  product_id = $2`, [userId, productId])).rows[0]
+        // update current record's quantity in basket
+        basketItem.quantity = quantity;
+        await client.query(`update baskets set quantity = $1 where ordered_by = $2`, [basketItem.quantity, userId])
+        res.json({
+            message: "Amend successfully"
+        })
+    } catch (error: any) {
+        res.status(500).end(error.message)
+        console.log(error)
+    }
 
 }
 
 
 
-async function removeBasketItem(req: express.Request, res: express.Response) { }
+export async function removeBasketItem(req: express.Request, res: express.Response) {
+    try {
+        let { productId } = req.body
+        let userId = req.session['userId']
+        await client.query(`delete from baskets where ordered_by = $1 and product_id = $2`, [userId, productId])
+        res.json({
+            message: "Ops"
+        })
+    } catch (error: any) {
+        res.status(500).end(error.message)
+        console.log(error)
+    }
+}
 
 
 
-async function clearBasket(req: express.Request, res: express.Response) { }
+export async function clearBasket(req: express.Request, res: express.Response) {
+    try {
+        let userId = req.session['userId']
+        await client.query(`delete from baskets where ordered_by = $1`, [userId])
+        res.json({
+            message: "Are you kidding?"
+        })
+    } catch (error: any) {
+        res.status(500).end(error.message)
+        console.log(error)
+    }
+}
