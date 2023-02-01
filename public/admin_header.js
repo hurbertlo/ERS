82cda4326af0f2b4b1091e5b2b0e7ea6aa795727
Kay Wong <enjoykayday@gmail.com>
2023-02-01 18:41:06 +0800

    KAY

--- public/admin_header.js
+++ public/admin_header.js
@@ -21,11 +21,12 @@ document.querySelector("#admin_header").innerHTML = /*html*/ `
          </button>
          <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
              <div class="navbar-nav mr-auto py-0">
-                 <a href="admin_order_details.html" class="nav-item nav-link">銷售紀錄</a>
-                 <a href="admin_chat.html" class="nav-item nav-link">對話</a>
+                 <a href="admin_panel.html" class="nav-item nav-link">首頁</a>
+                 <a href="chat.html" class="nav-item nav-link">即時對話</a>
                  <a href="admin_update_order_status.html" class="nav-item nav-link">交易狀態</a>
                  <a href="admin_update_products.html" class="nav-item nav-link">產品更新</a>
-                 <a href="admin_inventory.html" class="nav-item nav-link">存貨</a>
+                 <a href="admin_inventory.html" class="nav-item nav-link">存貨紀錄</a>
+                 <a href="admin_order_details.html" class="nav-item nav-link">銷售紀錄</a>
              </div>
              <!-- <div class="navbar-nav ml-auto py-0">
                  <a href="" class="nav-item nav-link">Login</a>
